88f53ab8d8c167d81140f61c3c9418513d58d128
kevin <kevinmartinez2107@gmail.com>
2020-10-07 16:57:30 -0500

    Se añadieron pruebas unitarias

--- problems/easy/3 - remove vowels/solved/vowels.py
+++ problems/easy/3 - remove vowels/solved/vowels.py
@@ -2,14 +2,27 @@
 #Input : what is your name ?
 #Output : wht s yr nm ?
 
-#Homemade whith love in python 2.7
+#Homemade whith love in python 3.8
+import unittest
 
-vowels=["a","e","i","o","u","A","E","I","O","U"]
-text = "Hello my name is kevin"
-print(text)
+def removeVowels(text):
+    vowels=["a","e","i","o","u","A","E","I","O","U"]
+    arg=""
+    for i in text: 
+        if i in vowels:
+         i=i
+        else:
+            arg=arg+i
+    return arg
 
-for i in text: 
-    if i in vowels:
-        i=i
-    else:
-        print i,
+class Vowels(unittest.TestCase):
+    def test(self):
+        actual  = removeVowels("what is your name ?")
+        expected = "wht s yr nm ?";
+        self.assertEqual(actual,expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
