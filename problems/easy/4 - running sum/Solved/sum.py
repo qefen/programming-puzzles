c29e5e30faa89ba3a1c40990dc926daa3491ee58
kevin <kevinmartinez2107@gmail.com>
2020-10-08 12:47:46 -0500

    Se agrego prueba unitaria

--- problems/easy/4 - running sum/Solved/sum.py
+++ problems/easy/4 - running sum/Solved/sum.py
@@ -7,12 +7,33 @@
 #Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 
 #Made in Python 3.8.5
+import unittest
 
-nums=[1,2,3,4]
-count =0
-print(nums)
+def runningSum(num):
+    count=1
+    while count< len(num):
+        num[count]+=num[count-1]
+        count+=1
+    return num
 
-for i in nums:
-    count=count+i
-    print(count, end=" ")
+class Sum(unittest.TestCase):
+    def test1(self):
+        actual=runningSum([1,2,3,4])
+        expected=[1,3,6,10]
+        self.assertEqual(actual,expected)
 
+class Sum(unittest.TestCase):
+    def test2(self):
+        actual=runningSum([1, 1, 1, 1, 1])
+        expected=[1, 2, 3, 4, 5]
+        self.assertEqual(actual,expected)
+
+class Sum(unittest.TestCase):
+    def test3(self):
+        actual=runningSum([3, 1, 2, 10, 1])
+        expected=[3, 4, 6, 16, 17]
+        self.assertEqual(actual,expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
