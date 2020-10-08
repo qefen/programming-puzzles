a20d4f40b70fdea9d6c6a234cee7486ada088db2
kevin <kevinmartinez2107@gmail.com>
2020-10-08 09:03:06 -0500

    Se modifico extencion de archivo

--- problems/easy/3 - remove vowels/remove_vowels_test.py
+++ problems/easy/3 - remove vowels/remove_vowels_test.py
@@ -1,19 +1,3 @@
-# Remove Vowels
-
-Given a string, remove the vowels from the string and return the string without vowels.
-
-## Examples
-
-```
-Input : what is your name ?
-Output : wht s yr nm ?
-```
-
-## Template
-
-_remove_vowels_test.py_
-
-```py
 import unittest
 
 def removeVowels(text):
